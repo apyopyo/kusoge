6584054dd74c85486433a6a21a5f87176a13b124
apyopyo <apyo13a@yahoo.co.jp>
2016-12-15 06:49:50 +0900

    kusogeコンテストのゲーム

---
+++ Assets/Scripts/NotesScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NotesScript : MonoBehaviour {
+
+	public int lineNum;
+    public GameObject ExploadObj;
+	private GameManager _gameManager;
+    private AudioSource _audioSource;
+    void Start(){
+		_gameManager = GameObject.Find ("GameManager").GetComponent<GameManager> ();
+	}
+
+	void Update () {
+		this.transform.position += Vector3.down * 10 * Time.deltaTime;
+		if (this.transform.position.y < -5.0f) {
+			Debug.Log("false");
+			Destroy (this.gameObject);
+		}
+	}
+
+
+	void OnTriggerStay2D(Collider2D other){
+        Debug.Log(lineNum);
+        switch (lineNum) {
+		case 0:
+			CheckInput (KeyCode.D);
+			break;
+		case 1:
+			CheckInput (KeyCode.F);
+			break;
+		case 2:
+			CheckInput (KeyCode.Space);
+			break;
+		case 3:
+			CheckInput (KeyCode.J);
+			break;
+		case 4:
+			CheckInput (KeyCode.K);
+			break;
+		default:
+			break;
+		}
+	}
+
+	void CheckInput(KeyCode key){
+		if (Input.GetKeyDown (key)) {
+			_gameManager.GoodTimingFunc (lineNum);
+            // 爆発
+            Instantiate(ExploadObj, transform.position, Quaternion.identity);
+            // 毎回探索するキチガイロジック
+            _audioSource = GameObject.Find("GoodSE").GetComponent<AudioSource>();
+            _audioSource.Play();
+            Destroy (this.gameObject);
+		}
+	}
+}
